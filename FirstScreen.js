a9bc280c81f24d4c362722bb2c053ceb785438dc
Thanh Nguyen <jaynguyen@Thanhs-MacBook-Pro.local>
2018-12-13 15:20:02 +0700

    test on ios device

---
+++ FirstScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+export default class FirstScreen extends Component {
+    
+    render() {
+        return(
+            <View style= {{flex: 1}}>
+                <Text> Thanh Nguyen</Text>
+            </View>
+        )
+    }
+}
